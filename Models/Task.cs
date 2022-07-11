a28f0a240ef2d9a8d6f3c39a2e2ba23714459367
Stephanie Owens <StephOwens2@gmail.com>
2022-07-11 12:32:02 -0400

    Add project files.

---
+++ Models/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Models
+{
+    public class Task
+    {
+        public int TaskID { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; } = false;
+    }
+}
